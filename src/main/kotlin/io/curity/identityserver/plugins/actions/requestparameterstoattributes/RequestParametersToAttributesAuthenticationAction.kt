c633836c561fe9011d51b6a0427a40c482e280a7
Daniel Lindau <d.lindau@gmail.com>
2024-03-25 14:21:21 +0100

    Add attribute sinks

--- src/main/kotlin/io/curity/identityserver/plugins/actions/requestparameterstoattributes/RequestParametersToAttributesAuthenticationAction.kt
+++ src/main/kotlin/io/curity/identityserver/plugins/actions/requestparameterstoattributes/RequestParametersToAttributesAuthenticationAction.kt
@@ -1,8 +1,29 @@
+/*
+ *   Copyright 2024 Curity AB
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package io.curity.identityserver.plugins.actions.requestparameterstoattributes
 
+import io.curity.identityserver.plugins.actions.requestparameterstoattributes.RequestParametersToAttributesAuthenticationActionConfig.AttributeLocation.ACTION_ATTRIBUTES
+import io.curity.identityserver.plugins.actions.requestparameterstoattributes.RequestParametersToAttributesAuthenticationActionConfig.AttributeLocation.CONTEXT_ATTRIBUTES
+import io.curity.identityserver.plugins.actions.requestparameterstoattributes.RequestParametersToAttributesAuthenticationActionConfig.AttributeLocation.SUBJECT_ATTRIBUTES
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import se.curity.identityserver.sdk.attribute.AuthenticationActionAttributes
 import se.curity.identityserver.sdk.attribute.AuthenticationAttributes
+import se.curity.identityserver.sdk.attribute.ContextAttributes
 import se.curity.identityserver.sdk.attribute.MapAttributeValue
 import se.curity.identityserver.sdk.attribute.SubjectAttributes
 import se.curity.identityserver.sdk.authenticationaction.AuthenticationAction
@@ -10,14 +31,15 @@ import se.curity.identityserver.sdk.authenticationaction.AuthenticationActionCon
 import se.curity.identityserver.sdk.authenticationaction.AuthenticationActionResult
 import se.curity.identityserver.sdk.web.Request
 
-class RequestParametersToAttributesAuthenticationAction(private val configuration: RequestParametersToAttributesAuthenticationActionConfig,
-                                                        private val request: Request): AuthenticationAction
-{
+class RequestParametersToAttributesAuthenticationAction(
+    private val configuration: RequestParametersToAttributesAuthenticationActionConfig,
+    private val request: Request
+) : AuthenticationAction {
     companion object {
         var logger: Logger = LoggerFactory.getLogger(RequestParametersToAttributesAuthenticationAction::class.java)
     }
-    override fun apply(context: AuthenticationActionContext): AuthenticationActionResult
-    {
+
+    override fun apply(context: AuthenticationActionContext): AuthenticationActionResult {
         logger.debug("Parameter names in request: " + request.parameterNames)
         val parameterAttributes = mutableMapOf<String, String>()
         configuration.requestParameterNames().forEach { parameterName ->
@@ -34,9 +56,35 @@ class RequestParametersToAttributesAuthenticationAction(private val configuratio
             }
         }
 
-        val subjectAttributes = context.authenticationAttributes.subjectAttributes.with(MapAttributeValue.of(parameterAttributes))
-        val authenticationAttributes = AuthenticationAttributes.of(SubjectAttributes.of(subjectAttributes),
-            context.authenticationAttributes.contextAttributes)
-        return AuthenticationActionResult.successfulResult(authenticationAttributes)
+        var actionAttributes: AuthenticationActionAttributes = AuthenticationActionAttributes.empty()
+        val authenticationAttributes = when (configuration.getAttributeLocation()) {
+            SUBJECT_ATTRIBUTES -> {
+                val subjectAttributes =
+                    context.authenticationAttributes.subjectAttributes.with(MapAttributeValue.of(parameterAttributes))
+                actionAttributes = context.actionAttributes
+                AuthenticationAttributes.of(
+                    SubjectAttributes.of(subjectAttributes),
+                    context.authenticationAttributes.contextAttributes
+                )
+            }
+
+            CONTEXT_ATTRIBUTES -> {
+                val contexAttributes =
+                    context.authenticationAttributes.contextAttributes.with(MapAttributeValue.of(parameterAttributes))
+                actionAttributes = context.actionAttributes
+                AuthenticationAttributes.of(
+                    context.authenticationAttributes.subjectAttributes,
+                    ContextAttributes.of(contexAttributes)
+                )
+            }
+
+            ACTION_ATTRIBUTES -> {
+                val attributes = context.actionAttributes.with(MapAttributeValue.of(parameterAttributes))
+                actionAttributes = AuthenticationActionAttributes.fromAttributes(attributes)
+                context.authenticationAttributes
+            }
+        }
+
+        return AuthenticationActionResult.successfulResult(authenticationAttributes, actionAttributes)
     }
 }
--- src/main/kotlin/io/curity/identityserver/plugins/actions/requestparameterstoattributes/RequestParametersToAttributesAuthenticationActionConfig.kt
+++ src/main/kotlin/io/curity/identityserver/plugins/actions/requestparameterstoattributes/RequestParametersToAttributesAuthenticationActionConfig.kt
@@ -1,11 +1,34 @@
+/*
+ *   Copyright 2024 Curity AB
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package io.curity.identityserver.plugins.actions.requestparameterstoattributes
 
 
 import se.curity.identityserver.sdk.config.Configuration
+import se.curity.identityserver.sdk.config.annotation.DefaultEnum
 import se.curity.identityserver.sdk.config.annotation.Description
 
 interface RequestParametersToAttributesAuthenticationActionConfig : Configuration {
     @Description("The parameter names to find from the request")
     fun requestParameterNames(): List<String>
 
+    @DefaultEnum("SUBJECT_ATTRIBUTES")
+    fun getAttributeLocation(): AttributeLocation
+
+   enum class AttributeLocation {
+       SUBJECT_ATTRIBUTES, CONTEXT_ATTRIBUTES, ACTION_ATTRIBUTES
+   }
 }
